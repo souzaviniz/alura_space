55efb315ef3f0dc0d1702cb01842e0908dea619d
souzaviniz <vinicius._.souza@hotmail.com>
2023-01-30 21:15:35 -0300

    aula banco de dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    
+    dados = {
+    1: {"nome": "Nebulosa de Carina", "legenda": "webbtelecope.org / nasa / james webb"},
+    2: {"nome": "Galaxia NGC 1079", "legenda": "nasa.org / NASA / hubble"}
+    }
+    
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
