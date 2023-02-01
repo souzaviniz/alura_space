32a4493f120de922dcdf2630db8ecbdf16c569fe
souzaviniz <vinicius._.souza@hotmail.com>
2023-01-31 21:12:51 -0300

    aula admin

--- galeria/views.py
+++ galeria/views.py
@@ -1,15 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Fotografia
 
 
 def index(request):
-    
-    dados = {
-    1: {"nome": "Nebulosa de Carina", "legenda": "webbtelecope.org / nasa / james webb"},
-    2: {"nome": "Galaxia NGC 1079", "legenda": "nasa.org / NASA / hubble"}
-    }
-    
-    return render(request, 'galeria/index.html', {"cards": dados})
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
