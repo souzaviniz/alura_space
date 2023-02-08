02826047c987679346bb5de92043901b8e04f2ed
souzaviniz <vinicius._.souza@hotmail.com>
2023-02-07 21:27:22 -0300

    aula usuarios

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -4,12 +4,73 @@ class LoginForms(forms.Form):
     nome_login=forms.CharField(
         label="Nome de Login",
         required=True,
-        max_length=100
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex: Joao Silva',
+            }
+        )
     )
 
     senha=forms.CharField(
         label="Senha",
         required=True,
         max_length=70,
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha',
+            }
+        ),
     )
+
+
+class CadastroForms(forms.Form):
+    nome_cadastro=forms.CharField(
+        label='Nome de Cadastro',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex: Joao Silva',
+            }
+        )
+    )
+
+    email=forms.EmailField(
+        label='Email',
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Ex: Joao@exemplo.com',
+            }
+        )
+    )
+
+    senha_1=forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha',
+            }
+        ),
+    )  
+
+    senha_2=forms.CharField(
+        label="Confirme sua senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite novamente sua senha',
+            }
+        ),
+    ) 
